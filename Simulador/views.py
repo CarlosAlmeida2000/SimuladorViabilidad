ba10c29e5b31f5eca48a7c74310a7152965bca3b
Henry Pérez <74738884+HenryGP14@users.noreply.github.com>
2022-02-23 01:49:44 -0500

    Cambio de un dato del modelo y plantilla de proyectos 80%

--- Simulador/views.py
+++ Simulador/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 # Create your views here.
 def vwIndex(request):
     return render(request, "simulador/proyecto.html")
 
+
 def vwFlujo(request):
     return render(request, "simulador/flujo_efectivo.html")
+
+# def listar_proyectos(request):
