3b3ed8ae1891ffad97f751c19f2ccd97b6abca86
Henry Pérez <74738884+HenryGP14@users.noreply.github.com>
2022-02-23 23:28:36 -0500

    Plantilla de proyectos finalizada y guardado de proyecto completa

--- Simulador/urls.py
+++ Simulador/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("flujo-efectivo/", views.vwFlujo, name="flujo_efectivo"),
     path("guardar-actividad/", views.vwGuardarActividad, name="guardar-actividad"),
     path("guardar-valor/", views.vwGuardarValor, name="guardar-valor"),
+    path("guardar-proyecto/",views.vwGuardarProyecto, name="guardar-proyecto"),
     # path("lista-proyectos", views.listar_proyectos, name="ls-proyectos"),
 ]
